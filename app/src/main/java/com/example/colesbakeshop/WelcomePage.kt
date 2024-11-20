e9b0ad045d5815150f5d40aadf6fb463c817acfb
Cole-showers7 <98387486+Cole-showers7@users.noreply.github.com>
2024-11-20 22:31:39 +0100

    Welcome page on start of the app. Add login adn signup buttons

--- app/src/main/java/com/example/colesbakeshop/WelcomePage.kt
+++ app/src/main/java/com/example/colesbakeshop/WelcomePage.kt
@@ -3,14 +3,38 @@ package com.example.colesbakeshop
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.colesbakeshop.ui.theme.ColesBakeShopTheme
 
 class WelcomePage : androidx.activity.ComponentActivity() {
@@ -36,5 +60,75 @@ class WelcomePage : androidx.activity.ComponentActivity() {
 
 @Composable
 fun Welcome(){
-    Text(text= "Page for sign up and log in")
+    val comforter1 = FontFamily(
+        Font(R.font.comforter1)
+    )
+    val poppinsBold= FontFamily(
+        Font(R.font.poppinsbold)
+    )
+    val welcomeCake= painterResource(id = R.drawable.customizedcake)
+    Column (
+        modifier= Modifier
+            .fillMaxSize(0.9f)
+            .padding(top = 30.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ){
+        Text(
+            text= "Cole's BakeShop",
+            color= Color.Black,
+            fontFamily = comforter1,
+            style = TextStyle(
+                fontSize = 34.sp,
+                fontWeight = FontWeight.Medium
+            )
+        )
+        Spacer(modifier = Modifier.height(26.dp))
+        Box(
+            modifier = Modifier
+                .fillMaxWidth(0.9f)
+                .height(513.dp)
+                .clip(RoundedCornerShape(16.dp))
+                .background(Color.White)
+                .shadow(
+                    elevation = 8.dp,
+                    shape = RoundedCornerShape(16.dp)
+                )
+        ) {
+            Image(
+                painter = welcomeCake,
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .graphicsLayer { alpha = 1f }
+                    .drawWithContent {
+                        drawContent()
+                        drawRect(
+                            brush = Brush.verticalGradient(
+                                colors = listOf(
+                                    Color.Transparent,
+                                    Color.Black.copy(alpha = 0.7f)
+                                ),
+                                startY = size.height / 2,
+                                endY = size.height
+                            )
+                        )
+                    },
+                contentScale = ContentScale.Crop
+            )
+
+            Text(
+                text = "Home of Sweet Treats",
+                color = Color.White,
+                fontFamily = poppinsBold,
+                style = TextStyle(
+                    fontSize = 27.sp,
+                ),
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(bottom = 16.dp)
+            )
+        }
+
+    }
+
 }
